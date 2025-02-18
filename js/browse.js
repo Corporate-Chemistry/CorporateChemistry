7059ab31401f1d5d718c8b581aa37d8055b9ae05
LunKEA2024 <maaq0001@stud.kea.dk>
2025-02-18 14:49:06 +0100

    ADDED: Noter

--- js/browse.js
+++ js/browse.js
@@ -2,6 +2,8 @@ fetch("https://dummyjson.com/users")
   .then((res) => res.json())
   .then(console.log);
 
+// Psuedo kode - Skal hente GENDER data fra index.html når man hopper fra index.html til browse.html
+
 document.querySelectorAll(".pref").forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("selected");
