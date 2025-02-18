3aa91b03d19a8e4e5bf401da2bc44a4c96ebd8eb
LunKEA2024 <maaq0001@stud.kea.dk>
2025-02-18 14:23:17 +0100

    ADDED: Filter knapperne og har fyldt dem ud og styled dem en lille smule

--- js/browse.js
+++ js/browse.js
@@ -1,3 +1,7 @@
+fetch("https://dummyjson.com/users")
+  .then((res) => res.json())
+  .then(console.log);
+
 document.querySelectorAll(".pref").forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("selected");
